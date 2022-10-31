d97b4fce6e21536ea74ee80923dad1556719cbaf
Braian <bryansalles45@gmail.com>
2022-10-31 20:11:58 -0300

    no comment

--- AgendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
+++ AgendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package agendatelefonica;
 
-/**
- *
- * @author brn
- */
-public class AgendaTelefonica {
 
-    /**
-     * @param args the command line arguments
-     */
+public class AgendaTelefonica {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
